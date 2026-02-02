300511c77155f13d46de1fd5b3f8e34f824793b3
Andres Gonzalez <agonzalez@azurian.com>
2026-02-01 20:17:54 -0600

    feat(auth):se agrega autenticacion y login funcional

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginPage } from "./modules/auth/login-page/login-page";
 
 @Component({
   selector: 'app-root',
