151d869d00d720678edf71d790f9d9573a35aad3
Andres Gonzalez <agonzalez@azurian.com>
2026-02-02 23:46:18 -0600

    feat:add chat-list

---
+++ src/app/core/models/chatsList.model.ts
@@ -0,0 +1,9 @@
+export interface ChatListItem {
+  id: number;
+  name: string;          // Nombre del grupo o usuario
+  lastMessage: string;   // "Hola..."
+  lastActivity: string;  // Fecha en formato ISO string
+  isGroup: boolean;      // true = Grupo, false = Privado
+  unreadCount?: number;  // Opcional
+  // avatar?: string;    // (Lo dejaremos pendiente, usaremos uno por defecto)
+}
