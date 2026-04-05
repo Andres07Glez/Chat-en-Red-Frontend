04c95e9416e48c46be439de9681e7844cb5097e5
Andres Gonzalez <manuel07andres@gmail.com>
2026-04-05 16:38:08 -0600

    chore:udates environments

--- src/app/config/environment.ts
+++ src/app/config/environment.ts
@@ -1,6 +1,7 @@
 export const environment= {
-  baseUrl: 'http://192.168.1.65:8181',
-  wsUrl: 'ws://192.168.1.65:8181/ws-chat/websocket',
+  production: false,
+  baseUrl: 'https://192.168.1.65:8181',
+  wsUrl: 'wss://192.168.1.65:8181/ws-chat/websocket',
   // baseUrl: 'http://localhost:8181',
   // wsUrl: 'ws://localhost:8181/ws-chat/websocket'
 };
